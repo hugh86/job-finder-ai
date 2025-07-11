fedc55aab28ce88e1575a4a03a6fc77a45fc3da0
hugh86 <hugosimmelink@gmail.com>
2025-07-10 22:50:10 -0700

    Update email_sender.py

--- utils/email_sender.py
+++ utils/email_sender.py
@@ -1,1 +1,6 @@
-#email
+def output_html(jobs, filename="job_matches.html"):
+    with open(filename, "w", encoding="utf-8") as f:
+        f.write("<html><body><h2>Matching Jobs</h2><ul>")
+        for job in jobs:
+            f.write(f"<li><strong>{job['title']}</strong><br>{job['description']}<br><em>{job.get('reason', '')}</em></li><br>")
+        f.write("</ul></body></html>")
