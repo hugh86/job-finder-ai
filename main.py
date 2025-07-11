97f4523dfdb3d637106efa54235185acfb33550c
hugh86 <hugosimmelink@gmail.com>
2025-07-10 20:33:11 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+import os
+from utils.job_scraper import get_mock_jobs
+from utils.ai_filter import filter_jobs
+from utils.email_sender import output_html
+
+RESUME_PATH = "resume.txt"
+
+def load_resume(path):
+    with open(path, "r", encoding="utf-8") as f:
+        return f.read()
+
+def main():
+    resume = load_resume(RESUME_PATH)
+    jobs = get_mock_jobs()
+    relevant_jobs = filter_jobs(resume, jobs)
+    output_html(relevant_jobs)
+
+if __name__ == "__main__":
+    main()
